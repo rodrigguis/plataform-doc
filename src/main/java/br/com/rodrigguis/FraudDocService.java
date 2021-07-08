1f7c0f8b2320f8ff9026e9eb098255babf658543
renato <renato.rodriguis01@gmail.com>
2021-07-08 15:13:40 -0300

    feat: add kakfa service, class-3 atv-2

--- src/main/java/br/com/rodrigguis/FraudDocService.java
+++ src/main/java/br/com/rodrigguis/FraudDocService.java
@@ -1,56 +1,32 @@
 package br.com.rodrigguis;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudDocService {
     final static String TOPIC = "PLATAFORM_NEW_DOC";
+    final static String GROUP_ID = FraudDocService.class.getSimpleName();
 
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(getProperties());
-        consumer.subscribe(Collections.singletonList(TOPIC));
-
-        while (true) {
-            final var records = consumer.poll(Duration.ofMillis(100));
-
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros.");
-                for (var messageRecords : records) {
-                    System.out.println("****************************************************");
-                    System.out.println("Processing new Order, checking for fraud");
-                    System.out.println("key:       " + messageRecords.key());
-                    System.out.println("value:     " + messageRecords.value());
-                    System.out.println("partition: " + messageRecords.partition());
-                    System.out.println("record:    " + messageRecords.offset());
-
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        Thread.currentThread().interrupt();
-                    }
-
-                    System.out.println("Order processed . . .");
-                }
-            }
-        }
+        var fraudDocService = new FraudDocService();
+        final var kafkaService = new KafkaService(GROUP_ID, TOPIC, fraudDocService::parse);
+        kafkaService.run();
     }
 
-    private static Properties getProperties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDocService.class.getName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDocService.class.getName() + UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+    private void parse(ConsumerRecord<String, String> messageRecord) {
+        System.out.println("****************************************************");
+        System.out.println("Processing new Order, checking for fraud");
+        System.out.println("key:       " + messageRecord.key());
+        System.out.println("value:     " + messageRecord.value());
+        System.out.println("partition: " + messageRecord.partition());
+        System.out.println("record:    " + messageRecord.offset());
 
-        return properties;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Order processed . . .");
     }
 }
---
+++ src/main/java/br/com/rodrigguis/KafkaService.java
@@ -0,0 +1,47 @@
+package br.com.rodrigguis;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+class KafkaService {
+
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
+
+    KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<String, String>(getProperties(groupId));
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    void run(){
+        while (true) {
+            final var records = consumer.poll(Duration.ofMillis(100));
+
+            if (!records.isEmpty()) {
+                System.out.println("Encontrei " + records.count() + " registros.");
+                for (var messageRecords : records) {
+                    parse.consume(messageRecords);
+                }
+            }
+        }
+    }
+
+    private static Properties getProperties(String groupId) {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+
+        return properties;
+    }
+}
